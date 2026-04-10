cb2cdad2fab161209a7e98a9685e9628487a86c6
Saleh Abdullahi <salehabdullahi018@gmail.com>
2026-04-10 15:27:47 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import donorRoutes from "./routes/donor.js";
 import { connectDB } from "./config/db.js";
 import * as middleware from "./utils/middleware.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(middleware.requestLogger);
 
 //Routes
